2dddd8637903c0331052829b50d7211e52489e4e
jcritoph <jcritoph@uoguelph.ca>
2025-02-10 16:03:36 -0500

    Fixing merge conflicts, adding filter by priority. Ref: #46

--- src/utils/filterManager.js
+++ src/utils/filterManager.js
@@ -2,12 +2,14 @@ class FilterManager {
   constructor() {
     this.filterCompletion = 'all';
     this.filterCategory = 'all';
+    this.filterPriority = 'all';
   }
 
   // Filters the to do list based on the selected filter method
   filterTasks(unfilteredTasks) {
     let filteredTasks = unfilteredTasks;
     
+    //Filtering for completion
     const filterCompletionElement = document.querySelector('.filter-completion');
     this.filterCompletion = filterCompletionElement.value;
     if (this.filterCompletion === 'pending') {
@@ -16,10 +18,18 @@ class FilterManager {
       filteredTasks = filteredTasks.filter(task => task.completed);
     }
     
+    //Filtering for Category
     const filterCategoryElement = document.querySelector('.filter-category');
     this.filterCategory = filterCategoryElement.value;
-    if (this.filterCategory != 'all'){
+    if (this.filterCategory != 'All' && this.filterCategory != 'all'){
       filteredTasks = filteredTasks.filter(task => this.filterCategory == task.category);
+    }
+
+    //Filtering for Priority
+    const filterPriorityElement = document.querySelector('.filter-priority');
+    this.filterPriority = filterPriorityElement.value;
+    if (this.filterPriority != 'All' && this.filterPriority != 'all'){
+      filteredTasks = filteredTasks.filter(task => this.filterPriority == task.priority);
     }
     
     return filteredTasks;
